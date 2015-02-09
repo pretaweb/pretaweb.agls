ebd413522c84d9af262467c40de51b373df5df3d
Ivan Teoh <ivan.teoh@gmail.com>
2015-02-09 13:25:48 +1100

    Fix the title and description used the default value from content and also remove content.Creator as it is sensitive info.

--- pretaweb/agls/browser/viewlets.py
+++ pretaweb/agls/browser/viewlets.py
@@ -14,32 +14,33 @@ from pretaweb.agls.config import AGLS_SCHEME
 
 class AGLSViewlet(DublinCoreViewlet):
     index = ViewPageTemplateFile('templates/agls.pt')
-    
+
     def update(self):
         super(AGLSViewlet, self).update()
-        
+
         # DC tags are switched off in Plone control panel
         if not self.metatags:
             return
-        
+
         # map dublin core meta tags
         dc = {}
         for tag in self.metatags:
             dc[tag[0]] = tag[1]
-        
+
         context = aq_inner(self.context)
         agls_tags = []
 
         agls = context.unrestrictedTraverse("@@agls")
+        # AGLS Title (mandatory)
         value = agls.Title() or dc.get('DC.Title', '') or context.Title()
         agls_tags.append({
             'name': u'DCTERMS.title',
-            'content': safe_unicode(agls.Title()),
+            'content': safe_unicode(value),
             'scheme': AGLS_SCHEME['DCTERMS.title']
         })
-        
-        # AGLS Description
-        value = agls.Description() or dc.get('DC.description', '')
+
+        # AGLS Description (mandatory)
+        value = agls.Description() or dc.get('DC.description', '') or context.Description()
         agls_tags.append({
             'name': u'DCTERMS.description',
             'content': safe_unicode(value),
@@ -50,41 +51,43 @@ class AGLSViewlet(DublinCoreViewlet):
             'content': safe_unicode(value),
             'scheme': AGLS_SCHEME['description']
         })
-        
-        # AGLS Date
+
+        # AGLS Date (mandatory)
         agls_tags.append({
             'name': u'DCTERMS.created',
             'content': agls.Created(),
             'scheme': AGLS_SCHEME['DCTERMS.created']
         })
-        
+
+        # AGLS Creator (mandatory)
         agls_tags.append({
             'name': u'DCTERMS.creator',
             'content': safe_unicode(agls.Creator() or dc.get('DC.creator', '')),
             'scheme': AGLS_SCHEME['DCTERMS.creator']
         })
 
+        # AGLS Subject
         value = agls.Subject() or '; '.join(dc.get('DC.subject', '').split(', '))
         agls_tags.append({
             'name': u'DCTERMS.subject',
             'content': safe_unicode(value),
             'scheme': AGLS_SCHEME['DCTERMS.subject']
         })
-        
-        # AGLS Type
+
+        # AGLS Type (mandatory)
         agls_tags.append({
             'name': u'DCTERMS.type',
             'content': safe_unicode(agls.Type()),
             'scheme': AGLS_SCHEME['DCTERMS.type']
         })
-        
-        # AGLS Identifier
+
+        # AGLS Identifier (mandatory)
         agls_tags.append({
             'name': u'DCTERMS.identifier',
             'content': agls.Identifier(),
             'scheme': AGLS_SCHEME['DCTERMS.identifier']
         })
-        
+
         # copy over keywords tag
         if 'keywords' in dc:
             agls_tags.append({
@@ -92,11 +95,11 @@ class AGLSViewlet(DublinCoreViewlet):
                 'content': safe_unicode(dc['keywords']),
                 'scheme': None
             })
-        
+
         # AGLS Publisher
         agls_tags.append({
             'name': u'DCTERMS.publisher',
-            'content': safe_unicode(agls.Publisher() or dc.get('DC.creator', '') or context.Creator()),
+            'content': safe_unicode(agls.Publisher() or dc.get('DC.creator', '')),
             'scheme': AGLS_SCHEME['DCTERMS.publisher']
         })
 
